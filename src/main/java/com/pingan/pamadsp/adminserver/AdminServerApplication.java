cde44bf598f48f8c61b1959c8e00bc77e588e3fc
folgerhuang <379771379@qq.com>
2018-09-06 19:38:36 +0800

    first commit

---
+++ src/main/java/com/pingan/pamadsp/adminserver/AdminServerApplication.java
@@ -0,0 +1,21 @@
+package com.pingan.pamadsp.adminserver;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@Configuration
+@EnableAdminServer //开启Spring Boot Admin 服务
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServerApplication.class, args);
+	}
+}
